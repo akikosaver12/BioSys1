807eec7036b1a97f6b128556fbaa543cf2889924
Manuel <manolosanchez2005@gmail.com>
2025-10-02 11:50:53 -0500

    fin

--- BioSys/backend/server.js
+++ BioSys/backend/server.js
@@ -148,7 +148,8 @@ const corsOptions = {
     'http://localhost:3000',
     'http://127.0.0.1:3000',
     'https://accounts.google.com',
-    'https://www.googleapis.com'
+    'https://www.googleapis.com',
+    'https://biosysvet.site' 
   ],
   credentials: true,
   methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
